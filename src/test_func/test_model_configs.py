5a0a36e76868688c68234e3054f7ef5e53d57b34
huguesroy <hugues.roy@gadz.org>
2026-02-02 11:52:33 +0100

    model config

--- src/test_func/test_model_configs.py
+++ src/test_func/test_model_configs.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 # add src/ to PYTHONPATH programmatically
 ROOT = Path(__file__).resolve().parent.parent
-sys.path.append(str(ROOT))
+sys.path.insert(0, str(ROOT))
 
-@hydra.main(config_path=f"/{ROOT}/models/fanogan/configs/model", config_name="gpgan", version_base=None)
+@hydra.main(config_path=f"/{ROOT}/models/cvae/configs", config_name="model/3D_cvae", version_base=None)
 def main(cfg: DictConfig):
     print("Loaded config:")
-    print(OmegaConf.to_yaml(cfg))
+    print(OmegaConf.to_yaml(cfg, resolve=True))
 
     # Hydra instantiates 
     #the model from _target_
@@ -21,4 +21,5 @@ def main(cfg: DictConfig):
 
 
 if __name__ == "__main__":
+    print(ROOT)
     main()
