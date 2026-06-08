bb48ff6650c6afb415c5225e272aa7c14ffd3e43
msolal <maelys.solal@cs.ox.ac.uk>
2026-06-08 14:02:54 +0200

    train config for revisions

--- train_model.sh
+++ train_model.sh
@@ -3,13 +3,12 @@
 #SBATCH --output=logs/train%j.out
 #SBATCH --constraint=a100
 #SBATCH --ntasks=1
-#SBATCH --array=0-4
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 #SBATCH --hint=nomultithread
 #SBATCH --account=krk@a100
 #SBATCH --qos=qos_gpu_a100-t3
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 
 # Activate your conda environment
 module load miniforge
@@ -24,6 +23,6 @@ CONFIG_NAME=train_vp
 cd /gpfswork/rech/krk/uqo89gi/projects/3DLatentDDPM/src
 
 # Run the Hydra training script
-echo HYDRA_FULL_ERROR=1 python train.py --config-path $CONFIG_PATH --config-name $CONFIG_NAME split=${SLURM_ARRAY_TASK_ID}
+echo HYDRA_FULL_ERROR=1 python train.py --config-path $CONFIG_PATH --config-name $CONFIG_NAME
 
-HYDRA_FULL_ERROR=1 python train.py --config-path $CONFIG_PATH --config-name $CONFIG_NAME split=${SLURM_ARRAY_TASK_ID}
+HYDRA_FULL_ERROR=1 python train.py --config-path $CONFIG_PATH --config-name $CONFIG_NAME
